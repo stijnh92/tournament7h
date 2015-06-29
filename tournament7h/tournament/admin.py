980f81e3b78c92ef3935b39b54fb1bdc557e924f
Stijn Houben <stijn.houben@dynapps.be>
2015-06-29 20:32:26 +0200

    PEP8 from atom

--- tournament7h/tournament/admin.py
+++ tournament7h/tournament/admin.py
@@ -4,24 +4,26 @@ from django.contrib import admin
 from django.contrib import admin
 from tournament.models import Player, Team, Group, Game, Goal, FairPlay, Location
 
+
 class GoalInline(admin.TabularInline):
     model = Goal
     classes = ('grp-collapse grp-closed',)
+
 
 class FairPlayInline(admin.TabularInline):
     model = FairPlay
     classes = ('grp-collapse grp-closed',)
 
+
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
     date_hierarchy = 'date'
-    list_display = ('date', 'group', 'team_home', 'team_away', 'amount_home', 'amount_away')
+    list_display = ('date', 'group', 'team_home', 'team_away',
+                    'amount_home', 'amount_away')
     list_filter = ('group', 'team_home', 'team_away')
     inlines = [
         GoalInline, FairPlayInline
     ]
-
-
 
 
 admin.site.register(Team)
